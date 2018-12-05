d679b8f1d97235f1144c65970b229fc63dc7421e
Andrew Day <day_andy@hotmail.com>
2018-12-05 22:34:12 +1100

    added google adds

--- src/Landing.js
+++ src/Landing.js
@@ -13,6 +13,7 @@ class Landing extends Component {
         <h2><a href="/ausozlotto">AUS OzLotto</a></h2>
         <h2><a href="/austattslotto">AUS Tattslotto</a></h2>
         <div align="center">Visitors: <a href='https://www.counter12.com'><img src='https://www.counter12.com/img-Z7z1Cdy9bxd4W7bC-2.gif' border='0' alt='acesso gratis'/></a><script type='text/javascript' src='https://www.counter12.com/ad.js?id=Z7z1Cdy9bxd4W7bC'></script></div>
+        
       </div>
     )
   }
